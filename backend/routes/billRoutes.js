2025f6d6aea9681c5f12557785cd1b147bd0fba6
Jatin-Catnip <jat.shah@catnipit.com>
2026-01-26 02:16:53 +0530

    remove commented code,minor debugging

--- backend/routes/billRoutes.js
+++ backend/routes/billRoutes.js
@@ -1,30 +1,3 @@
-// const express = require('express');
-// const router = express.Router();
-// const billController = require('../controllers/billController');
-// const { auth } = require('../middleware/auth');
-
-// // All routes require authentication
-// router.use(auth);
-
-// // Bill CRUD operations
-// router.post('/', billController.createBill);
-// router.get('/', billController.getAllBills);
-// router.get('/preview-number', billController.previewBillNumber);
-// router.get('/:billNo', billController.getBillByNumber);
-// router.put('/:id', billController.updateBill);
-// router.delete('/:id', billController.deleteBill);
-
-// // Bill actions
-// router.post('/:id/finalize', billController.finalizeBill);
-// router.get('/:id/pdf', billController.generatePDF);
-// router.post('/:id/email', billController.sendEmail);
-
-// // Service management
-// router.post('/:id/services', billController.addServiceToBill);
-// router.delete('/services/:serviceId', billController.deleteService);
-
-// module.exports = router;
-
 const express = require('express');
 const router = express.Router();
 const billController = require('../controllers/billController');
--- backend/routes/clientRoutes.js
+++ backend/routes/clientRoutes.js
@@ -1,23 +1,3 @@
-// const express = require('express');
-// const router = express.Router();
-// const clientController = require('../controllers/clientController');
-// const bulkImportController = require('../controllers/bulkImportController');
-// const { auth } = require('../middleware/auth');
-
-// // All routes require authentication
-// router.use(auth);
-
-// // Client CRUD operations
-// router.post('/', clientController.createClient);
-// router.get('/', clientController.getAllClients);
-// router.get('/search', clientController.searchClients);
-// router.get('/:id', clientController.getClientById);
-// router.put('/:id', clientController.updateClient);
-// router.delete('/:id', clientController.deleteClient);
-// router.post('/bulk-import', bulkImportController.bulkImportClients);
-
-// module.exports = router;
-
 const express = require('express');
 const router = express.Router();
 const clientController = require('../controllers/clientController');
